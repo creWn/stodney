dc9d3528718e7bb950cc2a932c2fb83f3cd60b75
creWn <dmitriy.parhomenko@gmail.com>
2017-05-04 23:03:14 +0300

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ var app = express();
 app.use(express.logger());
 app.set("view engine", "tpl");
 app.get('/', function(request, response) {
-  response.render("index.tpl");
+  response.render("index");
 });
 
 var port = process.env.PORT || 5000;
