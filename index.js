3d17a68ec59d43277147ca2b2030b3af1445df66
creWn <dmitriy.parhomenko@gmail.com>
2017-05-04 21:03:14 +0300

    Update index.js

--- index.js
+++ index.js
@@ -6,6 +6,16 @@ app.get('/', function(request, response) {
   response.send("<form><a href='/back'><button>Назад</button></a><a href='/forward'><button>Вперед</button></a></form>");
 });
 
+app.get('/back', function(request, response) {
+  console.log('hello world');
+  response.send("<form><a href='/back'><button>Назад</button></a><a href='/forward'><button>Вперед</button></a></form>");
+});
+
+app.get('/forward', function(request, response) {
+  console.log('hello world');
+  response.send("<form><a href='/back'><button>Назад</button></a><a href='/forward'><button>Вперед</button></a></form>");
+});
+
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
   console.log("Listening on " + port);
