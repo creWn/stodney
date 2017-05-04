2522d67c4d6dc420f42ba07abc80ab534943240f
creWn <dmitriy.parhomenko@gmail.com>
2017-05-04 23:08:04 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,12 +1,23 @@
-var express = require("express");
+var cool = require('cool-ascii-faces');
+var express = require('express');
 var app = express();
 
-app.set("view engine", "hbs");
+app.set('port', (process.env.PORT || 5000));
+
+app.use(express.static(__dirname + '/public'));
+
+// views is directory for all template files
+app.set('views', __dirname + '/views');
+app.set('view engine', 'ejs');
+
 app.get('/', function(request, response) {
-  response.render("index");
+  response.render('pages/index');
 });
 
-var port = process.env.PORT || 5000;
-app.listen(port, function() {
-  console.log("Listening on " + port);
+app.get('/*', function(request, response) {
+  response.send('404');
+});
+
+app.listen(app.get('port'), function() {
+  console.log('Node app is running on port', app.get('port'));
 });
