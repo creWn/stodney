b973c37662237040a02bdee0ece1f161db76ad64
creWn <dmitriy.parhomenko@gmail.com>
2017-05-04 20:47:35 +0300

    Update index.js

--- index.js
+++ index.js
@@ -3,10 +3,25 @@ var app = express();
 app.use(express.logger());
 
 app.get('/', function(request, response) {
-  response.send("<form><button>Назад</button><button>Вперед</button></form>");
+  response.send(index());
 });
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
+
+function index() {
+  res.writeHead(200, {'Content-Type': 'text/html;charset=utf-8'});
+  
+  res.write('<form>');
+  res.write('<button>');
+  res.write('Назад');
+  res.write('</button>');
+  res.write('<button>');
+  res.write('Вперед');
+  res.write('</button>');
+  res.write('</form>');
+
+  res.end();
+}
