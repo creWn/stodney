4399332d523c1fe057fe6d1ee4a4d8b34831715c
creWn <dmitriy.parhomenko@gmail.com>
2017-05-05 23:37:38 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,16 +1,10 @@
 var app = require('express')();
-var server = require('http').Server(app);
-var io = require('socket.io')(server);
+var http = require('http').Server(app);
 
-server.listen(80);
-
-app.get('/', function (req, res) {
-  res.sendfile(__dirname + '/index.html');
+app.get('/', function(req, res){
+  res.send('<h1>Hello world</h1>');
 });
 
-io.on('connection', function (socket) {
-  socket.emit('news', { hello: 'world' });
-  socket.on('my other event', function (data) {
-    console.log(data);
-  });
+http.listen(3000, function(){
+  console.log('listening on *:3000');
 });
