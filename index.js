d17496faa96061ed911719e44fb83565d0c38d04
creWn <dmitriy.parhomenko@gmail.com>
2017-05-05 23:49:19 +0300

    Update index.js

--- index.js
+++ index.js
@@ -13,6 +13,13 @@ app.get('/19', function(req, res) {
     res.sendFile(__dirname + '/days/19/index.html');
 });
 
+io.on('connection', function(socket) {
+    socket.on('chat message', function(msg) {
+        console.log('message: ' + msg);
+        io.emit('chat message', msg);
+    });
+});
+
 app.listen(app.get('port'), function() {
     console.log('Node app is running on port', app.get('port'));
 });
