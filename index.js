5c9435094c3210902e886d37c65664242e261cca
creWn <dmitriy.parhomenko@gmail.com>
2017-05-05 23:18:13 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,25 +1,16 @@
-var cool = require('cool-ascii-faces');
-var express = require('express');
-var app = express();
-var server = require('http').Server(app); // Подключаем http через app
-var io = require('socket.io')(server); // Подключаем socket.io и указываем на сервер
+var app = require('express')();
+var server = require('http').Server(app);
+var io = require('socket.io')(server);
 
-app.set('port', (process.env.PORT || 5000));
+server.listen(80);
 
-app.use(express.static(__dirname + '/public'));
-
-// views is directory for all template files
-app.set('views', __dirname + '/views');
-app.set('view engine', 'ejs');
-
-app.get('/', function(request, response) {
-  response.render('index');
+app.get('/', function (req, res) {
+  res.sendfile(__dirname + '/index.html');
 });
 
-app.get('/*', function(request, response) {
-  response.send('404');
-});
-
-app.listen(app.get('port'), function() {
-  console.log('Node app is running on port', app.get('port'));
+io.on('connection', function (socket) {
+  socket.emit('news', { hello: 'world' });
+  socket.on('my other event', function (data) {
+    console.log(data);
+  });
 });
