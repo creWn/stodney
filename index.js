165fdcca704ab82de35f58050cc1510a81ee69e5
creWn <dmitriy.parhomenko@gmail.com>
2017-05-06 23:02:42 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,20 +1,46 @@
-'use strict';
+"use strict";
 
-const express = require('express');
-const socketIO = require('socket.io');
-const path = require('path');
+let express = require('express');
+let app = express();
+let server = require('http').createServer(app);
+let io = require('socket.io')(server);
+let port = process.env.PORT || 8081;
 
-var app = express()
-  .use((req, res) => res.sendFile(INDEX) )
-.listen(PORT, () => console.log(`Listening on ${ PORT }`));
-
-const io = socketIO(server);
+app.use(express.static(__dirname + '/public'));
+app.set('views', __dirname + '/views');
+app.set('view engine', 'ejs');
 
 app.get('/', function(req, res) {
     res.send("Hello World!");
 });
 
-app.get('/20', function(req, res) {
-    res.sendFile(__dirname + '/days/20/index.html');
- 
+app.get('/20', function (req, res) {
+    res.render('days/20/index');
+});
+
+io.on('connection', function (socket) {
+    socket.on('19_send', function (data) {
+        socket.emit('19_listen', data);
+        socket.broadcast.emit('19_listen', data);
+    });
+
+    socket.on('20_command_send', function (data) {
+        socket.broadcast.emit('20_command_listen', data);
+    });
+
+    socket.on('21_command_send', function (data) {
+        data.socket_id = socket.id;
+        socket.broadcast.emit('21_command_listen', data);
+    });
+
+    socket.on('disconnect', function(){
+        socket.broadcast.emit('21_command_listen', {
+            socket_id: socket.id,
+            command: 'delete'
+        });
+    });
+});
+
+server.listen(port, function () {
+    console.log(`App listening on port ${port}`);
 });
