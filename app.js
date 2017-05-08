d93237b238422cae2ab91c4a2efbbef6a20e8fff
creWn <dmitriy.parhomenko@gmail.com>
2017-05-08 21:20:45 +0300

    Update app.js

--- app.js
+++ app.js
@@ -6,61 +6,49 @@ let server = require('http').createServer(app);
 let io = require('socket.io')(server);
 let port = process.env.PORT || 8081;
 
+
 app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
 
-app.get('/21', function (req, res) {
-    res.render('pages/21/index');
-});
-app.get('/21/controllers', function (req, res) {
-    res.render('pages/21/controllers');
+app.get('/', function (req, res) {
+    res.render('pages/index');
 });
 
-var array_sprites = {};
+app.get('/22', function (req, res) {
+    res.render('pages/22/server');
+});
+app.get('/22/mr', function (req, res) {
+    res.render('pages/22/client');
+});
+
+var coords = {};
 io.on('connection', function (socket) {
-    socket.on('19_send', function (data) {
-        socket.emit('19_listen', data);
-        socket.broadcast.emit('19_listen', data);
-    });
+    socket.emit('circlexy_listen', coords);
+    socket.broadcast.emit('circlexy_listen', coords);
+    socket.on('circlexy_send', function (data) {
 
-    socket.on('20_command_send', function (data) {
-        socket.broadcast.emit('20_command_listen', data);
-    });
-
-    socket.emit('21_command_listen', array_sprites);
-    socket.broadcast.emit('21_command_listen', array_sprites);
-    socket.on('21_command_send', function (data) {
-        if (socket.id in array_sprites) {
-            switch (data.command) {
-                case 'up':
-                    array_sprites[socket.id].y -= 10;
-                    break;
-                case 'down':
-                    array_sprites[socket.id].y += 10;
-                    break;
-                case 'right':
-                    array_sprites[socket.id].x += 10;
-                    break;
-                case 'left':
-                    array_sprites[socket.id].x -= 10;
-                    break;
-            }
+        if (socket.id in coords) {
+            coords[socket.id].x = data.x;
+            coords[socket.id].y = data.y;
+            coords[socket.id].color = data.color;
         } else {
-            array_sprites[socket.id] = {
-                x: 40,
-                y: 40
+            coords[socket.id] = {
+                x: 400,
+                y: 300,
+                color: 'red'
             };
         }
-
-        socket.broadcast.emit('21_command_listen', array_sprites);
+        socket.broadcast.emit('circlexy_listen', coords);
     });
 
+
     socket.on('disconnect', function(){
-        delete array_sprites[socket.id];
-        socket.broadcast.emit('21_command_listen', array_sprites);
+        delete coords[socket.id];
+        socket.broadcast.emit('circlexy_listen', coords);
     });
 });
 
 server.listen(port, function () {
     console.log(`App listening on port ${port}`);
 });
+
