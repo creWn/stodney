ce30afc8047fa7b38707daf4fce281331cbe59b1
creWn <dmitriy.parhomenko@gmail.com>
2017-05-20 00:28:03 +0300

    Update app.js

--- app.js
+++ app.js
@@ -14,88 +14,55 @@ app.get('/', function (req, res) {
     res.render('pages/index');
 });
 
-app.get('/22', function (req, res) {
-    res.render('pages/22/server');
-});
-app.get('/22/mr', function (req, res) {
-    res.render('pages/22/client');
-});
-app.get('/23', function (req, res) {
-    res.render('pages/22/server');
-});
-app.get('/24', function (req, res) {
-    res.render('pages/24/server');
-});
-app.get('/28', function (req, res) {
-    res.render('pages/28/index');
-});
-app.get('/29', function (req, res) {
-    res.render('pages/29/index');
-});
-app.get('/30', function (req, res) {
-    res.render('pages/30/index');
-});
-app.get('/31', function (req, res) {
-    res.render('pages/31/index');
-});
-app.get('/32', function (req, res) {
-    res.render('pages/32/index');
+app.get('/33', function (req, res) {
+    res.render('pages/33/index');
 });
 
-var coords = {};
+var rooms = [];
 
-io.on('connection', function (socket) {
-    socket.emit('circlexy_listen', coords);
-    socket.broadcast.emit('circlexy_listen', coords);
-    socket.on('circlexy_send', function (data) {
+io.on('connection', function(socket) {
 
-        if (socket.id in coords) {
-            coords[socket.id].x = data.x;
-            coords[socket.id].y = data.y;
-            coords[socket.id].color = data.color;
-            coords[socket.id].nick = data.nick;
-            coords[socket.id].res = data.res;
-        } else {
-            coords[socket.id] = {
-                x: 400,
-                y: 300,
-                color: 'red',
-                res: 10
-            };
-        }
-        socket.broadcast.emit('circlexy_listen', coords);
-    });
+	socket.on('get rooms', function() {
+		socket.emit('send rooms', rooms);
+	});
 
-    var room = '';
-    socket.on('room_id', function (data) {
-        room = data;
-        socket.join(data);
-    });
+	socket.on('create room', function(room, user) {
+		var roomObj = {
+			name: room,
+			users: [user]
+		};
+		rooms.push(roomObj);
+		socket.join(room);
+		socket.emit('send users', roomObj.users);
+	});
 
-    socket.in('H3R0Ku').emit('console', 'H3R0Ku');
+	socket.on('join to room', function(room, user) {
+		var roomUsers = null;
+		rooms.forEach(function(roomItem) {
+			if (roomItem.name === room) {
+				roomItem.users.push(user);
+				roomUsers = roomItem.users;
+			};
+		})
+		socket.join(room);
+		socket.to(room).emit('add user', user);
+		socket.emit('send users', roomUsers);
+	});
 
-    socket.on('29_send', function (data) {
-        if (socket.id in coords) {
-            coords[socket.id].x = data.x;
-            coords[socket.id].y = data.y;
-            coords[socket.id].color = data.color;
-            coords[socket.id].size = data.size;
-        } else {
-            coords[socket.id] = {
-                x: 400,
-                y: 300,
-                color: 'red',
-                size: 40
-            };
-        }
-        socket.in(room).broadcast.emit('29_get', coords);
+	socket.on('send coord', function(user) {
+		socket.to(user.room).emit('send coord', user);
+	});
 
-    });
-    
-    socket.on('disconnect', function(){
-        delete coords[socket.id];
-        socket.broadcast.emit('circlexy_listen', coords);
-    });
+	socket.on('disconnect', function() {
+		rooms.forEach(function(room) {
+			room.users.forEach(function(user, index) {
+				if (user.id === socket.id) {
+					room.users.splice(index, 1);
+					socket.to(room.name).emit('delete user', user);
+				}
+			})
+		});
+	});
 });
 
 server.listen(port, function () {
